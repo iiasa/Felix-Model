ee05ee0b349fd6ffa86c8210147a009829146df1
yequanliang <yequanliang1993@gmail.com>
2025-02-04 13:52:01 +0100

    cfps data cleaning and analysis

---
+++ scripts/ageing_society_cfps/cfps_analysis.py
@@ -0,0 +1,428 @@
+# -*- coding: utf-8 -*-
+"""
+Created: Wednesday 08 January 2025
+Description: Scripts to analyze corelationships between consumption and household education and age
+Scope: Ageing society project, module cfps_data
+Author: Quanliang Ye
+Institution: Radboud University
+Email: quanliang.ye@ru.nl
+"""
+
+import datetime
+import json
+import logging
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+timestamp = datetime.datetime.now()
+file_timestamp = timestamp.ctime()
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Specify the log message format
+    datefmt="%Y-%m-%d %H:%M:%S",  # Specify the date format
+    handlers=[
+        logging.StreamHandler(),  # Log to console
+        logging.FileHandler("app.log"),  # Log to a file
+    ],
+)
+
+# Configure paths
+root_path = Path("./cfps_data")
+path_data_clean = root_path / "cfps_clean"
+
+# specify a survey year
+year = "2020"
+
+logging.info(f"Load cleaned survey data for the year {year}")
+cleaned_fam_consum = pd.read_csv(path_data_clean / f"cfps_household_data_{year}.csv")
+
+consum_columns = [
+    "daily",  # Household equipment and daily necessities expenditure
+    "dress",  # Clothing and shoes expenditure
+    "eec",  # Education and entertainment expenditure
+    "food",  # Food expenditure
+    "house",  # Residential expenditure
+    "med",  # Health care expenditure
+    "trco",  # Transportation and communication expenditure
+    "other",  # Other expenditure
+]
+
+
+# calculate the average age of the household
+def weighted_average_age(household_row: pd.Series):
+    """
+    To calculate weighted average age of each household
+        - adult older than 20 (including), weight 1
+        - person age between 10 (including) and 19 (including), weight 0.5
+        - person younger than 10, weight 0.3
+
+    Input parameter:
+    household_row: pd.Series
+        Each row from the family consumption DataFrame
+
+    Return:
+        average_age
+
+    """
+    age_columns = [
+        column_name for column_name in household_row.keys() if "age_in_" in column_name
+    ]
+
+    household_ages = household_row[age_columns]
+    edu_weights = np.select(
+        [
+            household_ages >= 20,
+            (household_ages >= 10) & (household_ages <= 19),
+            household_ages < 10,
+        ],
+        [1, 0.5, 0.3],
+        default=0,  # Default weight for NaN
+    )
+    household_ages_valid = household_ages[~household_ages.isna()]  # Exclude NaN values
+    edu_weights_valid = edu_weights[~household_ages.isna()]  # Corresponding weights
+    if len(household_ages_valid) == 0:  # Handle households with no valid ages
+        return np.nan
+    return np.sum(household_ages_valid * edu_weights_valid) / np.sum(edu_weights_valid)
+
+
+# calculate household size
+def weighted_household_size(household_row: pd.Series):
+    """
+    To calculate household size each household, using Dependency Ratio. Weights are
+        - adult older than 65 (including), weight 0.7
+        - person age between 20 (including) and 64 (including), weight 1
+        - person age between 10 (including) and 19 (including), weight 0.5
+        - person younger than 9 (including), weight 0.3
+
+    Input parameter:
+    household_row: pd.Series
+        Each row from the family consumption DataFrame
+
+    Return:
+        household_size
+    """
+    age_columns = [
+        column_name for column_name in household_row.keys() if "age_in_" in column_name
+    ]
+
+    household_ages = household_row[age_columns]
+    household_size_weights = np.select(
+        [
+            household_ages >= 65,
+            (household_ages >= 20) & (household_ages < 65),
+            (household_ages >= 10) & (household_ages < 20),
+            household_ages < 10,
+        ],
+        [0.7, 1, 0.5, 0.3],
+        default=0,  # Default weight for NaN
+    )
+    household_ages_valid = household_ages[~household_ages.isna()]  # Exclude NaN values
+    household_size_weights_valid = household_size_weights[
+        ~household_ages.isna()
+    ]  # Corresponding weights
+    if len(household_ages_valid) == 0:  # Handle households with no valid ages
+        return np.nan
+    return np.sum(household_size_weights_valid)
+
+
+# Function to calculate the maximal and average educational level of the household
+def cal_educational_level(household_row: pd.Series):
+    """
+    To calculate maxinmal and weighted average educational level of each household
+        - adult older than 65 (including), educational level weight 0.7
+        - person age between 20 (including) and 64 (including), educational level weight 1
+        - person age between 10 (including) and 19 (including), educational level weight 0.5
+        - person younger than 9 (including), educational level weight 0.3
+
+    Input parameter:
+    household_data: pd.DataFrame
+        The family consumption DataFrame
+
+    Return:
+        pd.DataFrame() with two new columns representing maximal and weighted average educational level
+
+    """
+    # logging.info("Specify column names with educational levels")
+    # person_edu_columns = [
+    #     column_name
+    #     for column_name in household_data.columns
+    #     if "edu_pid_" in column_name
+    # ]
+    # logging.info("Specify column names of age")
+    # age_columns = [
+    #     column_name
+    #     for column_name in household_data.columns
+    #     if "age_in_" in column_name
+    # ]
+
+    # # Define a function to calculate weight based on age
+    # def calculate_weight(age):
+    #     if pd.isna(age):
+    #         return np.nan
+    #     elif age >= 65:
+    #         return 0.7
+    #     elif age >= 20:
+    #         return 1
+    #     elif age >= 10:
+    #         return 0.5
+    #     elif age < 10:
+    #         return 0.3
+
+    # logging.info("Apply weights according to person's age")
+    # for age_column in age_columns:
+    #     household_data[f"weight_{age_column}"] = household_data[age_column].apply(
+    #         calculate_weight
+    #     )
+
+    # logging.info("Calculate maximal and weighted average educational level")
+    # for pos, (person_column, age_column) in enumerate(
+    #     zip(person_columns, age_columns), start=1
+    # ):
+    #     weight_column = f"weight_{age_column}"
+    #     total_weights = household_data[
+    #         [f"weight_{age_col}" for age_col in age_columns]
+    #     ].sum(axis=1)
+    #     for category in consum_columns:
+    #         household_data[f"{person_column}_{category}"] = (
+    #             household_data[category] * household_data[weight_column] / total_weights
+    #         )
+
+    # logging.info("Reshape the household dataset")
+    # per_capita_consum_data = []
+    # for pos, (person_column, age_column) in enumerate(
+    #     zip(person_columns, age_columns), start=1
+    # ):
+    #     temp_household_data = household_data[
+    #         [f"fid{year[2:]}", f"provcd{year[2:]}", person_column, age_column]
+    #         + [f"{person_column}_{consum_column}" for consum_column in consum_columns]
+    #     ].copy()
+
+    #     temp_household_data.columns = [
+    #         f"fid{year[2:]}",
+    #         f"provcd{year[2:]}",
+    #         "pid",
+    #         f"age_in_{year}",
+    #     ] + [f"{consum_column}_per_capita" for consum_column in consum_columns]
+    #     per_capita_consum_data.append(temp_household_data)
+
+    # reshaped_per_capita_consum_data = (
+    #     pd.concat(per_capita_consum_data).dropna(subset=["pid"]).set_index("pid")
+    # )
+
+    # return reshaped_per_capita_consum_data
+    age_columns = [
+        column_name for column_name in household_row.keys() if "age_in_" in column_name
+    ]
+
+    household_edu_levels = household_row[person_edu_columns]
+    household_ages = household_row[age_columns]
+    edu_weights = np.select(
+        [
+            household_ages >= 65,
+            (household_ages >= 20) & (household_ages < 65),
+            (household_ages >= 10) & (household_ages < 20),
+            household_ages < 10,
+        ],
+        [0.7, 1, 0.5, 0.3],
+        default=0,  # Default weight for NaN
+    )
+    household_edu_valid = household_edu_levels[
+        ~household_edu_levels.isna()
+    ]  # Exclude NaN values
+    edu_weights_valid = edu_weights[
+        ~household_edu_levels.isna()
+    ]  # Corresponding weights
+    if len(household_edu_valid) == 0:  # Handle households with no valid ages
+        return np.nan
+    return np.sum(household_edu_valid * edu_weights_valid) / np.sum(edu_weights_valid)
+
+
+# Function to calculate per-capita consumption
+def weighted_per_capita_consum(household_data: pd.DataFrame):
+    """
+    To calculate per-capita consumption of each person, weights are:
+        - adult older than 65 (including), weight 0.7
+        - person age between 20 (including) and 64 (including), weight 1
+        - person age between 10 (including) and 19 (including), weight 0.7
+        - person younger than 9 (including), weight 0.5
+
+    Input parameter:
+    household_data: pd.DataFrame
+        The family consumption DataFrame
+
+    Return:
+        household_size
+    """
+
+    logging.info("Specify column names with personal id (pid)")
+    person_columns = [
+        column_name
+        for column_name in household_data.columns
+        if "pid_pid_" in column_name
+    ]
+    logging.info("Specify column names of age")
+    age_columns = [
+        column_name for column_name in household_data.columns if "age_in" in column_name
+    ]
+
+    # Define a function to calculate weight based on age
+    def calculate_weight(age):
+        if pd.isna(age):
+            return np.nan
+        elif age >= 65:
+            return 0.7
+        elif age >= 20:
+            return 1
+        elif age >= 10:
+            return 0.5
+        elif age < 10:
+            return 0.3
+
+    logging.info("Apply weights according to person's age")
+    for age_column in age_columns:
+        household_data[f"weight_{age_column}"] = household_data[age_column].apply(
+            calculate_weight
+        )
+
+    logging.info("Calculate per-capita consumption for each person and category")
+    for pos, (person_column, age_column) in enumerate(
+        zip(person_columns, age_columns), start=1
+    ):
+        weight_column = f"weight_{age_column}"
+        total_weights = household_data[
+            [f"weight_{age_col}" for age_col in age_columns]
+        ].sum(axis=1)
+        for category in consum_columns:
+            household_data[f"{person_column}_{category}"] = (
+                household_data[category] * household_data[weight_column] / total_weights
+            )
+
+    logging.info("Reshape the household dataset")
+    per_capita_consum_data = []
+    for pos, (person_column, age_column) in enumerate(
+        zip(person_columns, age_columns), start=1
+    ):
+        temp_household_data = household_data[
+            [f"fid{year[2:]}", f"provcd{year[2:]}", person_column, age_column]
+            + [f"{person_column}_{consum_column}" for consum_column in consum_columns]
+        ].copy()
+
+        temp_household_data.columns = [
+            f"fid{year[2:]}",
+            f"provcd{year[2:]}",
+            "pid",
+            f"age_in_{year}",
+        ] + [f"{consum_column}_per_capita" for consum_column in consum_columns]
+        per_capita_consum_data.append(temp_household_data)
+
+    reshaped_per_capita_consum_data = (
+        pd.concat(per_capita_consum_data).dropna(subset=["pid"]).set_index("pid")
+    )
+
+    return reshaped_per_capita_consum_data
+
+
+logging.info("Calculate weighed average age")
+cleaned_fam_consum["average_age"] = cleaned_fam_consum.apply(
+    weighted_average_age, axis=1
+)
+
+logging.info("Calculate weighed household size")
+cleaned_fam_consum["household_size"] = cleaned_fam_consum.apply(
+    weighted_household_size, axis=1
+)
+
+logging.info("Calculate maximal and weighed educational level of the household")
+person_edu_columns = [
+    column_name
+    for column_name in cleaned_fam_consum.columns
+    if "edu_pid_" in column_name
+]
+cleaned_fam_consum["average_edu"] = cleaned_fam_consum.apply(
+    cal_educational_level, axis=1
+)
+
+cleaned_fam_consum["max_edu"] = cleaned_fam_consum[person_edu_columns].max(axis=1)
+cleaned_fam_consum.to_csv("test_cleaned_fam_consum.csv", index=False)
+
+
+logging.info("Calculate weighted consumption by person")
+cleaned_person_consum = weighted_per_capita_consum(household_data=cleaned_fam_consum)
+
+
+logging.info("Save data")
+file_name = f"cfps_household_consum_with_average_age_size_{year}.csv"
+cleaned_fam_consum.to_csv(path_data_clean / file_name, index=False)
+del file_name
+
+file_name = f"cfps_personal_consum_by_category_{year}.csv"
+cleaned_person_consum.to_csv(path_data_clean / file_name, index=False)
+del file_name
+
+# # Exploratory Data Analysis (EDA)
+# plt.scatter(cleaned_fam_consum["average_edu"], cleaned_fam_consum["eec"])
+# plt.xlabel("Average Educational Level")
+# plt.ylabel("Household Consumption")
+# plt.title("Relationship between Average Age and Consumption")
+# plt.show()
+
+
+# # Statistical Analysis
+# # correlation = cleaned_fam_consum[["average_age", "pce"]].corr()
+# # print(correlation)
+
+# logging.info("Omit rows with no average age data")
+# cleaned_fam_consum = cleaned_fam_consum.dropna(subset=["average_age"])
+
+# logging.info("Fill missing total consumption data")
+# cleaned_fam_consum["pce"] = cleaned_fam_consum["pce"].fillna(
+#     cleaned_fam_consum[consum_columns].sum(axis=1, skipna=True)
+# )
+# logging.info("Omit rows with no total consumption data")
+# cleaned_fam_consum = cleaned_fam_consum.dropna(subset=["pce"])
+
+# x_average_age = cleaned_fam_consum["average_age"].values.reshape(-1, 1)
+# y_total_consum = cleaned_fam_consum["pce"]
+
+# # # Linear regression
+# # model = LinearRegression()
+# # model.fit(x_average_age, y_total_consum)
+
+# # print(f"Intercept: {model.intercept_}")
+# # print(f"Coefficient: {model.coef_[0]}")
+
+# # Non-linear regression
+# poly_model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
+# poly_model.fit(x_average_age, y_total_consum)
+
+# # Accessing the PolynomialFeatures step
+# poly_features = poly_model.named_steps["polynomialfeatures"]
+# print("Polynomial Features Attributes:")
+# print(f"Degree: {poly_features.degree}")
+# print(f"Include Bias: {poly_features.include_bias}")
+
+# # Accessing the LinearRegression step
+# linear_model = poly_model.named_steps["linearregression"]
+# print("\nLinear Regression Attributes:")
+# print(f"Coefficients: {linear_model.coef_}")
+# print(f"Intercept: {linear_model.intercept_}")
+
+
+# # Extract the fitting function
+# def fitting_function(x):
+#     # Polynomial expansion: [1, x, x^2]
+#     expanded_x = poly_features.transform(np.array([[x]]))
+#     return np.dot(expanded_x, linear_model.coef_) + linear_model.intercept_
+
+
+# print("\nFitting Function:")
+# coeffs = linear_model.coef_
+# print(f"f(x) = {coeffs[0]} + {coeffs[1]}*x + {coeffs[2]}*x^2")
---
+++ scripts/ageing_society_cfps/cfps_ridge_plot.py
@@ -0,0 +1,405 @@
+# -*- coding: utf-8 -*-
+"""
+Created: Friday 10 January 2025
+Description: Scripts to make plots to show data
+Scope: Ageing society project, module cfps_data
+Author: Quanliang Ye
+Institution: Radboud University
+Email: quanliang.ye@ru.nl
+"""
+import datetime
+import json
+import logging
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+import matplotlib.patches as patches
+from matplotlib.lines import Line2D
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
+from cmcrameri import cm
+
+
+timestamp = datetime.datetime.now()
+file_timestamp = timestamp.ctime()
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level
+    format="%(asctime)s - %(levelname)s - %(message)s",  # Specify the log message format
+    datefmt="%Y-%m-%d %H:%M:%S",  # Specify the date format
+    handlers=[
+        logging.StreamHandler(),  # Log to console
+        logging.FileHandler("app.log"),  # Log to a file
+    ],
+)
+
+# Configure paths
+root_path = Path("./cfps_data")
+path_data_clean = root_path / "cfps_clean"
+
+# specify a survey year
+year = "2020"
+
+logging.info(f"Load cleaned survey data for the year {year}")
+input_file_name = f"cfps_household_consum_with_average_age_size_{year}.csv"
+fam_consum = pd.read_csv(path_data_clean / input_file_name)
+
+logging.info("Specify categories of consumption")
+consum_columns = [
+    "daily",  # Household equipment and daily necessities expenditure
+    "dress",  # Clothing and shoes expenditure
+    "eec",  # Education and entertainment expenditure
+    "food",  # Food expenditure
+    "house",  # Residential expenditure
+    "med",  # Health care expenditure
+    "trco",  # Transportation and communication expenditure
+    "other",  # Other expenditure
+]
+
+logging.info("Specific age cohort")
+age_cohorts = [f"{(x-1)*5}-{(x-1)*5+4}" for x in range(5, 21)] + ["100+"]
+
+
+#########################################################################
+# ridge plot
+#########################################################################
+fam_consum_no_nan = pd.DataFrame()
+for age_cohort in age_cohorts:
+    try:
+        age_start = int(age_cohort.split("-")[0])
+    except ValueError:
+        age_start = int(age_cohort.split("+")[0])
+    try:
+        age_end = int(age_cohort.split("-")[-1])
+    except ValueError:
+        age_end = 150
+
+    # subset the data for each word
+    subset_fam_consum = fam_consum[
+        (fam_consum["average_age"] >= age_start)
+        & (fam_consum["average_age"] < age_end + 1)
+    ].reset_index(drop=True)
+    subset_fam_consum["age_cohort"] = age_cohort
+    fam_consum_no_nan = pd.concat(
+        [fam_consum_no_nan, subset_fam_consum], ignore_index=True
+    )
+num_cohorts = len(np.unique(fam_consum_no_nan["age_cohort"]))
+
+logging.info("Make the plot")
+
+
+logging.info("Specify colormap and colors")
+darkgreen = "#9BC184"
+midgreen = "#C2D6A4"
+lightgreen = "#E7E5CB"
+colors = [lightgreen, midgreen, darkgreen, midgreen, lightgreen]
+fill_colors = [
+    cm.batlow.colors[index * int(256 / num_cohorts)] for index in range(0, num_cohorts)
+]
+
+logging.info("Specify font size and line width")
+font_size = 9
+line_width = 1.5
+
+logging.info("Specify width of band")
+bandwidth = 1
+
+
+fig, axs = plt.subplots(nrows=num_cohorts, ncols=1, figsize=(8, 8))
+axs = axs.flatten()  # needed to access each individual axis
+
+# logging.info("Add backgrounf lines")
+
+
+# def add_line(xpos, ypos, fig=fig):
+#     line = Line2D(xpos, ypos, color="lightgrey", lw=0.5, transform=fig.transFigure)
+#     fig.lines.append(line)
+
+
+# add_line([4.95 / 12.9, 4.95 / 12.9], [0.11, 0.88])
+# add_line([6.61 / 12.9, 6.61 / 12.9], [0.11, 0.88])
+# add_line([8.28 / 12.9, 8.28 / 12.9], [0.11, 0.88])
+# add_line([9.95 / 12.9, 9.95 / 12.9], [0.11, 0.88])
+# add_line([11.58 / 12.9, 11.58 / 12.9], [0.11, 0.88])
+
+
+for pos, age_cohort in enumerate(np.unique(fam_consum_no_nan["age_cohort"])):
+    # subset the data for each word
+    subset_fam_consum = fam_consum_no_nan[
+        fam_consum_no_nan["age_cohort"] == age_cohort
+    ].reset_index(drop=True)
+
+    sns.kdeplot(
+        subset_fam_consum["household_size"],
+        shade=True,
+        bw_adjust=bandwidth,
+        ax=axs[pos],
+        color=fill_colors[pos],
+        edgecolor=fill_colors[pos],
+        linewidth=line_width,
+    )
+
+    # national mean reference line
+    median_hh_size_chn = fam_consum["household_size"].median()
+    axs[pos].axvline(median_hh_size_chn, color="#525252", linestyle="--")
+
+    # display average number of bedrooms on left
+    axs[pos].text(
+        -1,
+        0,
+        f"'{age_cohort}'",
+        ha="left",
+        fontsize=font_size,
+        color="black",
+    )
+
+    # compute quantiles
+    quantiles = np.percentile(
+        subset_fam_consum["household_size"], [2.5, 10, 25, 75, 90, 97.5]
+    )
+    quantiles = quantiles.tolist()
+
+    # fill space between each pair of quantiles
+    for j in range(len(quantiles) - 1):
+        axs[pos].fill_between(
+            [quantiles[j], quantiles[j + 1]],  # lower bound  # upper bound
+            0,  # max y=0
+            0.4,  # max y=0.0002
+            color="white",
+            alpha=0.7,
+            edgecolor=colors[j],
+            linewidth=line_width,
+        )
+
+    # mean value as a reference
+    median_hh_size = subset_fam_consum["household_size"].median()
+    axs[pos].scatter([median_hh_size], [0.2], color="black", s=10)
+
+    # set title and labels
+    axs[pos].set_xlim(0, 12)
+    axs[pos].set_ylim(0, 2)
+    axs[pos].set_ylabel("")
+
+    # x axis scale for last ax
+    if pos == num_cohorts - 1:
+        values_ = [0, 2, 4, 6, 8, 10, 12]
+        for value_ in values_:
+            axs[pos].text(value_, -0.6, f"{value_}", ha="center", fontsize=font_size)
+
+    # remove axis
+    axs[pos].set_axis_off()
+
+# reference line label
+text = f"Median Line, {round(median_hh_size_chn,1)} persons per household"
+fig.text(0.29, 0.88, text, ha="left", fontsize=font_size)
+
+# number of bedrooms label
+text = "Age Cohort"
+fig.text(
+    0.04,
+    0.88,
+    text,
+    ha="left",
+    fontsize=font_size,
+    color="black",
+)
+
+# x axis label
+text = "Household size (persons per household)"
+fig.text(0.5, 0.07, text, ha="center", fontsize=font_size)
+
+subax = inset_axes(
+    parent_axes=axs[num_cohorts - 2],  # the number_cohorts-2 row
+    width="60%",
+    height="250%",
+    loc=4,
+)
+subax.set_xticks([])
+subax.set_yticks([])
+legend_subset = fam_consum_no_nan[
+    fam_consum_no_nan["age_cohort"] == "35-39"
+].reset_index(drop=True)
+sns.kdeplot(
+    legend_subset["household_size"],
+    shade=True,
+    ax=subax,
+    bw_adjust=2,
+    color="grey",
+    edgecolor="lightgrey",
+)
+quantiles = np.percentile(legend_subset["household_size"], [2.5, 10, 25, 75, 90, 97.5])
+quantiles = quantiles.tolist()
+for j in range(len(quantiles) - 1):
+    subax.fill_between(
+        [quantiles[j], quantiles[j + 1]],  # lower bound  # upper bound
+        0,  # max y=0
+        0.1,  # max y=0.00004
+        color="white",
+        alpha=0.7,
+        edgecolor=colors[j],
+        linewidth=line_width,
+    )
+subax.set_xlim(-0.9, 12)
+subax.set_ylim(-0.2, 0.4)
+legend_subset_mean = legend_subset["household_size"].median()
+subax.scatter([legend_subset_mean], [0.05], color="black", s=10)
+subax.text(0, 0.32, "Legend", ha="left", fontsize=font_size, weight="bold")
+subax.text(
+    7,
+    0.15,
+    "Distribution\nof hosuehold size",
+    ha="left",
+    fontsize=font_size,
+)
+
+
+logging.info("Add percentages and arrows in the legend")
+
+
+def add_arrow(head_pos, tail_pos, ax):
+    style = "Simple, tail_width=0.01, head_width=1.5, head_length=2.5"
+    kw = dict(arrowstyle=style, color="k", linewidth=line_width)
+    arrow = patches.FancyArrowPatch(
+        tail_pos, head_pos, connectionstyle="arc3,rad=.5", **kw
+    )
+    ax.add_patch(arrow)
+
+
+subax.text(
+    legend_subset_mean + 1,
+    0.16,
+    "Median",
+    ha="center",
+    fontsize=font_size,
+)
+add_arrow((legend_subset_mean, 0.06), (legend_subset_mean + 0.3, 0.15), subax)  # median
+
+
+subax.text(
+    8.1,
+    -0.16,
+    "95% of sizes",
+    ha="center",
+    fontsize=font_size,
+)
+add_arrow((6.5, 0), (7, -0.11), subax)  # 95%
+
+subax.text(
+    5,
+    -0.16,
+    "80% of sizes",
+    ha="center",
+    fontsize=font_size,
+)
+add_arrow((4.9, 0), (5, -0.11), subax)  # 80%
+
+subax.text(
+    1,
+    -0.17,
+    "50% of sizes \n within this range",
+    ha="center",
+    fontsize=font_size,
+)
+add_arrow((legend_subset_mean - 0.4, 0), (legend_subset_mean - 1, -0.11), subax)  # 50%
+
+
+plt.savefig("figure_household_size.png", dpi=300, bbox_inches="tight")
+plt.show()
+
+
+# sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+# # Create the data
+# rs = np.random.RandomState(1979)
+# x = rs.randn(500)
+# g = np.tile(list("ABCDEFGHIJ"), 50)
+# df = pd.DataFrame(dict(x=x, g=g))
+# m = df.g.map(ord)
+# df["x"] += m
+
+# fam_consum_age_cohorts = pd.DataFrame()
+# for pos, age_cohort in enumerate(age_cohorts):
+#     try:
+#         age_start = int(age_cohort.split("-")[0])
+#     except ValueError:
+#         age_start = int(age_cohort.split("+")[0])
+#     try:
+#         age_end = int(age_cohort.split("-")[-1])
+#     except ValueError:
+#         age_end = 150
+
+#     # subset the data for each word
+#     subset_fam_consum = fam_consum[
+#         (fam_consum["average_age"] >= age_start)
+#         & (fam_consum["average_age"] < age_end + 1)
+#     ].reset_index(drop=True)
+#     subset_fam_consum["age_cohort"] = age_cohort
+#     fam_consum_age_cohorts = pd.concat(
+#         [fam_consum_age_cohorts, subset_fam_consum],
+#         ignore_index=True,
+#     )
+#     del age_cohort, pos
+
+# num_cohorts = len(np.unique(fam_consum_age_cohorts["age_cohort"]))
+
+
+# # Initialize the FacetGrid object
+# pal = sns.cubehelix_palette(n_colors=num_cohorts, rot=-0.25, light=0.7)
+# g = sns.FacetGrid(
+#     fam_consum_age_cohorts[["household_size", "age_cohort"]],
+#     row="age_cohort",
+#     hue="age_cohort",
+#     aspect=15,
+#     height=0.5,
+#     palette=pal,
+# )
+
+# # Draw the densities in a few steps
+# g.map(
+#     sns.kdeplot,
+#     "household_size",
+#     bw_adjust=0.5,
+#     clip_on=False,
+#     fill=True,
+#     alpha=1,
+#     linewidth=1.5,
+# )
+# g.map(sns.kdeplot, "household_size", clip_on=False, color="w", lw=2, bw_adjust=0.5)
+
+# # passing color=None to refline() uses the hue mapping
+# g.refline(y=0, linewidth=2, linestyle="-", color=None, clip_on=False)
+
+
+# # Define and use a simple function to label the plot in axes coordinates
+# def label(x, color, label):
+#     ax = plt.gca()
+#     ax.text(
+#         0,
+#         0.2,
+#         label,
+#         fontweight="bold",
+#         color=color,
+#         ha="left",
+#         va="center",
+#         transform=ax.transAxes,
+#     )
+
+
+# g.map(label, "household_size")
+
+# # Set the subplots to overlap
+# g.figure.subplots_adjust(hspace=-0.25)
+
+# # Remove axes details that don't play well with overlap
+# g.set_titles("")
+# g.set(yticks=[], ylabel="")
+# g.despine(bottom=True, left=True)
+
+# plt.show()
